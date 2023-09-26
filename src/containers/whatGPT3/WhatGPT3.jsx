2cefd543cc54c5e8d01644ed921e18e092ede546
Rayy-007 <minkhant88887@gmail.com>
2023-09-26 09:09:44 +0630

    Created and Styled Brand

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import "./whatGPT3.css";
+import Features from "../features/Features";
 
 function WhatGPT3() {
-  return <div>WhatGPT3</div>;
+  return (
+    <div id="whgpt3" className="gpt3__whatgpt3 section__margin">
+      <div className="gpt3__whatgpt3-feature">
+        <Features />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient-text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Features />
+        <Features />
+        <Features />
+      </div>
+    </div>
+  );
 }
 
 export default WhatGPT3;
